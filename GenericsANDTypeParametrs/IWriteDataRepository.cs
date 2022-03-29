e7551afe281f80a9d4eec34224795550707a4cc4
Sona <52357447+SonaArshakyan@users.noreply.github.com>
2022-03-29 17:56:31 +0400

    Update IWriteDataRepository.cs

--- GenericsANDTypeParametrs/IWriteDataRepository.cs
+++ GenericsANDTypeParametrs/IWriteDataRepository.cs
@@ -1,6 +1,6 @@
 ﻿namespace GenericsANDTypeParametrs
 {
-    internal interface IWriteDataRepository<in T> //more derived
+    internal interface IWriteDataRepository<in T> // it enables to return  less derived types that specified by generic parameter
     {
         void AddData(T data);
         void DeleteData(T data);
